ffcf8a8f2ae31ec7973194eb471b7add7363386c
Nitisha <nitishamali23@gmail.com>
2026-05-16 11:21:20 +0530

    Fix syntax error in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Node.js App Running on ECS');             
-});
+});                                                                     
 
 const PORT = 3000;
 
